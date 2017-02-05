a6c9e182decae14381200761c58feeebbe938904
Wojciech Migda <wojtek.golf@interia.pl>
2017-02-05 21:13:03 +0100

    removed dead code

--- src/CMap2.hpp
+++ src/CMap2.hpp
@@ -42,20 +42,6 @@
 
 using size_type = std::size_t;
 
-//#define TESTING
-//
-//#ifdef TESTING
-//#include "array2d.hpp"
-//#include <utility>
-//#include <valarray>
-//using real_type = double;
-//using array_type = num::array2d<real_type>;
-//using varray_type = std::valarray<real_type>;
-//
-//std::vector<std::string>
-//read_file(const char * fname);
-//#endif
-
 #include <sys/time.h>
 long int timestamp()
 {
@@ -192,18 +178,12 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
             double const upenalty = -1. / (NGENES - QUSIZE);
             double wkts_up = 0.;
             {
-                //volatile
                 double _acc = 0.;
                 double _extr = 0.;
                 int prev = 0; // rank indices are 1-based
                 for (auto const & rs : up_ranks)
                 {
                     _acc += (rs.first - prev - 1) * upenalty;
-                    //asm("popcnt %rax,%rax");
-//                    for (auto aix = 0u; aix < (rs.first - prev - 1); ++aix)
-//                    {
-//                        _acc += penalty;
-//                    }
                     auto abs_acc = std::abs(_acc);
                     if (abs_acc > _extr)
                     {
@@ -220,16 +200,6 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
                         wkts_up = _acc;
                     }
                 }
-//                if (prev != NGENES)
-//                {
-//                    _acc += (NGENES - prev) * penalty;
-//                    auto const abs_acc = std::abs(_acc);
-//                    if (abs_acc > _extr)
-//                    {
-//                        _extr = abs_acc;
-//                        wkts_up = _acc;
-//                    }
-//                }
             }
 
             double const dpenalty = -1. / (NGENES - QDSIZE);
@@ -257,16 +227,6 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
                         wkts_dn = _acc;
                     }
                 }
-//                if (prev != NGENES)
-//                {
-//                    _acc += (NGENES - prev) * penalty;
-//                    auto const abs_acc = std::abs(_acc);
-//                    if (abs_acc > _extr)
-//                    {
-//                        _extr = abs_acc;
-//                        wkts_dn = _acc;
-//                    }
-//                }
             }
 
             auto wkts = (wkts_dn * wkts_up < 0.) ? (wkts_up - wkts_dn) / 2 : 0.;
@@ -285,137 +245,5 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
 
     return ret;
 }
-
-////////////////////////////////////////////////////////////////////////////////
-
-//std::vector<double>
-//CMAP2::getWTKScomb_(std::vector<std::string> & q_up, std::vector<std::string> & q_dn)
-//{
-//    constexpr auto NCOL = 10;
-//    constexpr auto NROW = 10174;
-//    using rank_type = std::size_t;
-//
-//#ifdef TESTING
-//    auto scores_vs = read_file("../data/code/data/score_n10x10174.csv");
-//    namespace q = ::cpplinq;
-//    auto score_headers_str = scores_vs.front().substr(scores_vs.front().find(',') + 1);
-//    std::cout << "score_headers_str : " << score_headers_str << "\n";
-//    std::unordered_map<int, int> gene_to_idx;
-//    q::from(scores_vs)
-//        >> q::skip(1)
-//        >> q::select([](std::string const & s){ return s.substr(0, s.find(',')); })
-//        >> q::zip_with(q::detail::int_range(0, INT_MAX))
-//        >> q::aggregate(&gene_to_idx, [](std::unordered_map<int, int> * seed, std::pair<std::string, int> const & kv){ seed->emplace(std::stoi(kv.first), kv.second); return seed; })
-//        ;
-//    std::cout << "Gene '100' index shall be 0 : " << gene_to_idx.at(100) << "\n";
-//    auto string_as_varray = [NCOL](std::string const & s)
-//        {
-//            real_type row[NCOL];
-//            sscanf(s.c_str(), "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf", row + 0, row + 1, row + 2, row + 3, row + 4, row + 5, row + 6, row + 7, row + 8, row + 9);
-//            return varray_type(row, NCOL);
-//        };
-//    auto varray_reducer = [](array_type * seed, std::pair<varray_type, int> const & zipped)
-//        {
-//            (*seed)[seed->row(zipped.second)] = zipped.first;
-//            return seed;
-//        };
-//    array_type scores(num::shape_type(NROW, NCOL), 0.f);
-//    q::from(scores_vs)
-//        >> q::skip(1)
-//        >> q::select([](std::string const & s){ return s.substr(s.find(',') + 1); })
-//        >> q::select(string_as_varray)
-//        >> q::zip_with(q::detail::int_range(0, INT_MAX))
-//        >> q::aggregate(&scores, varray_reducer)
-//        ;
-//    std::cout << "first row of scores : ";
-//    varray_type row0 = scores[scores.row(0)];
-//    std::copy(std::begin(row0), std::end(row0), std::ostream_iterator<real_type>(std::cout, ", "));
-//    std::cout << '\n';
-//
-//    num::array2d<rank_type> ranks(num::shape_type(NROW, NCOL));
-//    for (size_type ix = 0; ix < NCOL; ++ix)
-//    {
-//        varray_type col = scores[scores.column(ix)];
-//        auto r = q::from(std::vector<double>(std::begin(col), std::end(col)))
-//            >> q::zip_with(q::detail::int_range(0, INT_MAX))
-//            >> q::orderby_descending([](std::pair<double, int> const & vi){ return vi.first; })
-//            >> q::zip_with(q::detail::int_range(1, INT_MAX))
-//            >> q::orderby_ascending([](std::pair<std::pair<double, int>, int> const & vii){ return vii.first.second; })
-//            >> q::select([](std::pair<std::pair<double, int>, int> const & vii) -> rank_type { return vii.second; })
-//            >> q::to_vector(NROW)
-//            ;
-//        ranks[ranks.column(ix)] = std::valarray<rank_type>(r.data(), r.size());
-//    }
-//
-//#endif
-//
-//    auto q_up_parsed = parse_query(q_up.front());
-//    auto q_up_indices = q::from(q_up_parsed)
-//        >> q::select([&gene_to_idx](std::uint32_t gene) -> size_type { return gene_to_idx.at(gene); })
-//        >> q::to_vector(q_up_parsed.size())
-//        ;
-//
-//    std::valarray<rank_type> up_ranks = static_cast<std::valarray<rank_type>>(ranks[ranks.column(0)])[std::valarray<size_type>(q_up_indices.data(), q_up_indices.size())];
-//    std::valarray<double> run_stats(-1. / (NROW - q_up_parsed.size()), NROW);
-//    std::valarray<double> up_abs_scores = std::abs<double>(static_cast<std::valarray<double>>(scores[scores.column(0)])[std::valarray<size_type>(q_up_indices.data(), q_up_indices.size())]);
-//    auto const Sum_up_abs_scores = up_abs_scores.sum();
-//    run_stats[up_ranks] = up_abs_scores / Sum_up_abs_scores;
-//
-//    double _acc = 0.;
-//    double _extr = 0.;
-//    double wkts_up = 0.;
-//    auto it = std::begin(run_stats);
-//    _acc = *it++;
-//    wkts_up = _acc;
-//    _extr = std::abs(_acc);
-//    for (; it != std::end(run_stats); ++it)
-//    {
-//        _acc += *it;
-//        auto const abs_acc = std::abs(_acc);
-//        if (abs_acc > _extr)
-//        {
-//            _extr = abs_acc;
-//            wkts_up = _acc;
-//        }
-//    }
-//    std::cout << "wkts_up : " << wkts_up << "\n";
-//
-//
-//    auto q_dn_parsed = parse_query(q_dn.front());
-//    auto q_dn_indices = q::from(q_dn_parsed)
-//        >> q::select([&gene_to_idx](std::uint32_t gene) -> size_type { return gene_to_idx.at(gene); })
-//        >> q::to_vector(q_dn_parsed.size())
-//        ;
-//
-//    std::valarray<rank_type> dn_ranks = static_cast<std::valarray<rank_type>>(ranks[ranks.column(0)])[std::valarray<size_type>(q_dn_indices.data(), q_dn_indices.size())];
-//    run_stats = -1. / (NROW - q_dn_parsed.size());
-//    std::valarray<double> dn_abs_scores = std::abs<double>(static_cast<std::valarray<double>>(scores[scores.column(0)])[std::valarray<size_type>(q_dn_indices.data(), q_dn_indices.size())]);
-//    auto const Sum_dn_abs_scores = dn_abs_scores.sum();
-//    run_stats[dn_ranks] = dn_abs_scores / Sum_dn_abs_scores;
-//
-//    _acc = 0.;
-//    _extr = 0.;
-//    double wkts_dn = 0.;
-//    it = std::begin(run_stats);
-//    _acc = *it++;
-//    wkts_dn = _acc;
-//    _extr = std::abs(_acc);
-//    for (; it != std::end(run_stats); ++it)
-//    {
-//        _acc += *it;
-//        auto const abs_acc = std::abs(_acc);
-//        if (abs_acc > _extr)
-//        {
-//            _extr = abs_acc;
-//            wkts_dn = _acc;
-//        }
-//    }
-//    std::cout << "wkts_dn : " << wkts_dn << "\n";
-//
-//    auto wkts = (wkts_dn * wkts_up < 0.) ? (wkts_up - wkts_dn) / 2 : 0.;
-//    std::cout << "wkts : " << wkts << "\n";
-//
-//    return std::vector<double>();
-//}
 
 #endif /* SRC_CMAP2_HPP_ */
