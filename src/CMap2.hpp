e49ea16c5ec545c4b11aa39b64dbb1fd81a1a8b3
Wojciech Migda <wojtek.golf@interia.pl>
2017-02-06 23:57:12 +0100

    attempt with traversed sorted signature, slower, with mismatches

--- src/CMap2.hpp
+++ src/CMap2.hpp
@@ -99,138 +99,120 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
     assert(q_up.size() == q_dn.size());
     auto const NQRY = q_up.size();
 
-    auto q_indexer = [this](std::vector<std::string> & qry)
+    std::vector<bool> qmasks[2 * NQRY];
+    for (auto & m : qmasks)
+    {
+        m.resize(NGENES);
+    }
+
+    std::vector<std::vector<std::uint32_t>> q_up_indices(NQRY);
+    std::vector<std::vector<std::uint32_t>> q_dn_indices(NQRY);
+
+    for (auto qix = 0u; qix < NQRY; ++qix)
+    {
+        q_up_indices[qix] = parse_query(q_up[qix]);
+        auto & q_up_parsed = q_up_indices[qix];
+        std::transform(q_up_parsed.begin(), q_up_parsed.end(), q_up_parsed.begin(),
+            [this](int gene_id){ return this->m_gene_to_idx.at(gene_id); });
+
+        q_dn_indices[qix] = parse_query(q_dn[qix]);
+        auto & q_dn_parsed = q_dn_indices[qix];
+        std::transform(q_dn_parsed.begin(), q_dn_parsed.end(), q_dn_parsed.begin(),
+            [this](int gene_id){ return this->m_gene_to_idx.at(gene_id); });
+
+        for (auto gix : q_up_parsed)
         {
-            namespace q = ::cpplinq;
-            return q::from(qry)
-                >> q::select([this](std::string const & s)
-                    {
-                        auto q_parsed = parse_query(s);
-                        std::transform(q_parsed.begin(), q_parsed.end(), q_parsed.begin(),
-                            [this](int gene_id){ return this->m_gene_to_idx.at(gene_id); });
-                        return q_parsed;
-                    })
-                >> q::to_vector();
-        };
-
-    auto const q_up_indexed = q_indexer(q_up);
-    auto const q_dn_indexed = q_indexer(q_dn);
-
-    std::vector<double> sig;
-    std::vector<int> ranks;
+            qmasks[2 * qix + 0][gix] = true;
+        }
+        for (auto gix : q_dn_parsed)
+        {
+            qmasks[2 * qix + 1][gix] = true;
+        }
+    }
 
     std::vector<double> ret;
     ret.reserve(q_up.size() * NSIG);
 
+    std::vector<double> sig;
+    std::vector<int> ranks;
+
     for (auto six = 0u; six < NSIG; ++six)
     {
         sig = m_cmap_lib.loadFromDoubleFile(PATH + "scoresBySig", six * NGENES, NGENES);
-        ranks = m_cmap_lib.loadFromIntFile(PATH + "ranksBySig", six * NGENES, NGENES);
-        // cache i/o ???
+        //ranks = m_cmap_lib.loadFromIntFile(PATH + "ranksBySig", six * NGENES, NGENES);
+
+        namespace q = ::cpplinq;
+        auto scores_ranks = q::from(sig)
+            //>> q::select([](double x){ return std::abs(x); })
+            >> q::zip_with(q::detail::int_range(0, INT_MAX))
+            >> q::orderby_descending([](std::pair<double, int> const & p){ return /*std::abs*/(p.first); })
+            //>> q::select([](std::pair<double, int> const & p){ return p.second; })
+            >> q::to_vector(sig.size())
+            ;
+
 
         for (auto qix = 0u; qix < NQRY; ++qix)
         {
-            auto & q_up_indices = q_up_indexed[qix];
-            auto & q_dn_indices = q_dn_indexed[qix];
+            auto const Sum_up_abs_scores = std::accumulate(
+                q_up_indices[qix].cbegin(), q_up_indices[qix].cend(), 0.,
+                [&sig](double acc, int ix){ return acc + std::abs(sig[ix]); });
+            auto const Sum_dn_abs_scores = std::accumulate(
+                q_dn_indices[qix].cbegin(), q_dn_indices[qix].cend(), 0.,
+                [&sig](double acc, int ix){ return acc + std::abs(sig[ix]); });
 
-            auto const QUSIZE = q_up_indices.size();
-            auto const QDSIZE = q_dn_indices.size();
-
-            std::vector<std::pair<int, double>> up_ranks;
-            up_ranks.reserve(QUSIZE);
-            double Sum_up_abs_scores = 0.;
-            std::transform(q_up_indices.cbegin(), q_up_indices.cend(), std::back_inserter(up_ranks),
-                [&ranks, &sig, &Sum_up_abs_scores](int ix)
-                {
-                    auto abs_score = std::abs(sig.at(ix));
-                    Sum_up_abs_scores += abs_score;
-                    return std::make_pair(ranks.at(ix), abs_score);
-                });
-
-            std::vector<std::pair<int, double>> dn_ranks;
-            dn_ranks.reserve(QDSIZE);
-            double Sum_dn_abs_scores = 0.;
-            std::transform(q_dn_indices.cbegin(), q_dn_indices.cend(), std::back_inserter(dn_ranks),
-                [&ranks, &sig, &Sum_dn_abs_scores](int ix)
-                {
-                    auto abs_score = std::abs(sig.at(ix));
-                    Sum_dn_abs_scores += abs_score;
-                    return std::make_pair(ranks.at(ix), abs_score);
-                });
-
-            std::qsort(&up_ranks[0], up_ranks.size(), sizeof (up_ranks.front()),
-                [](const void * avp, const void * bvp)
-                {
-                    auto ap = static_cast<std::pair<int, double> const *>(avp);
-                    auto bp = static_cast<std::pair<int, double> const *>(bvp);
-                    return (ap->first > bp->first) - (ap->first < bp->first);
-                }
-            );
-            std::qsort(&dn_ranks[0], dn_ranks.size(), sizeof (dn_ranks.front()),
-                [](const void * avp, const void * bvp)
-                {
-                    auto ap = static_cast<std::pair<int, double> const *>(avp);
-                    auto bp = static_cast<std::pair<int, double> const *>(bvp);
-                    return (ap->first > bp->first) - (ap->first < bp->first);
-                }
-            );
+            auto const QUSIZE = q_up_indices[qix].size();
+            auto const QDSIZE = q_dn_indices[qix].size();
 
             double const upenalty = -1. / (NGENES - QUSIZE);
-            double wkts_up = 0.;
-            {
-                double _acc = 0.;
-                double _extr = 0.;
-                int prev = 0; // rank indices are 1-based
-                for (auto const & rs : up_ranks)
-                {
-                    _acc += (rs.first - prev - 1) * upenalty;
-                    auto abs_acc = std::abs(_acc);
-                    if (abs_acc > _extr)
-                    {
-                        _extr = abs_acc;
-                        wkts_up = _acc;
-                    }
-                    prev = rs.first;
-
-                    _acc += rs.second / Sum_up_abs_scores;
-                    abs_acc = std::abs(_acc);
-                    if (abs_acc > _extr)
-                    {
-                        _extr = abs_acc;
-                        wkts_up = _acc;
-                    }
-                }
-            }
-
             double const dpenalty = -1. / (NGENES - QDSIZE);
-            double wkts_dn = 0.;
-            {
-                double _acc = 0.;
-                double _extr = 0.;
-                int prev = 0; // rank indices are 1-based
-                for (auto const & rs : dn_ranks)
-                {
-                    _acc += (rs.first - prev - 1) * dpenalty;
-                    auto abs_acc = std::abs(_acc);
-                    if (abs_acc > _extr)
-                    {
-                        _extr = abs_acc;
-                        wkts_dn = _acc;
-                    }
-                    prev = rs.first;
 
-                    _acc += rs.second / Sum_dn_abs_scores;
-                    abs_acc = std::abs(_acc);
-                    if (abs_acc > _extr)
-                    {
-                        _extr = abs_acc;
-                        wkts_dn = _acc;
-                    }
+            double wkts_up = 0.;
+            double acc_up = 0.;
+            double extr_up = 0.;
+
+            double wkts_dn = 0.;
+            double acc_dn = 0.;
+            double extr_dn = 0.;
+
+            for (auto ix = 0u; ix < NGENES; ++ix)
+            {
+                auto const & score_rank = scores_ranks[ix];
+                auto const gix = score_rank.second;
+
+                bool is_up_qry = qmasks[qix * 2 + 0][gix];
+                bool is_dn_qry = qmasks[qix * 2 + 1][gix];
+
+                if (is_up_qry)
+                {
+                    acc_up += std::abs(score_rank.first) / Sum_up_abs_scores;
+                }
+                else
+                {
+                    acc_up += upenalty;
+                }
+
+                if (is_dn_qry)
+                {
+                    acc_dn += std::abs(score_rank.first) / Sum_dn_abs_scores;
+                }
+                else
+                {
+                    acc_dn += dpenalty;
+                }
+
+                if (std::abs(acc_up) > extr_up)
+                {
+                    extr_up = std::abs(acc_up);
+                    wkts_up = acc_up;
+                }
+                if (std::abs(acc_dn) > extr_dn)
+                {
+                    extr_dn = std::abs(acc_dn);
+                    wkts_dn = acc_dn;
                 }
             }
-
             auto wkts = (wkts_dn * wkts_up < 0.) ? (wkts_up - wkts_dn) / 2 : 0.;
-            //std::cout << "wkts : " << wkts << "\n";
+
             ret.push_back(wkts);
             if (m_gt)
             {
@@ -242,7 +224,6 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
             }
         }
     }
-
     return ret;
 }
 
