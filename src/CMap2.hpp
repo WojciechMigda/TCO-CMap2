e95e16d1d47067e8f597480b367b17f60c072541
Wojciech Migda <wojtek.golf@interia.pl>
2017-02-10 02:30:09 +0100

    packed f64/index

--- src/CMap2.hpp
+++ src/CMap2.hpp
@@ -65,7 +65,6 @@ struct CMAP2Updated
 
     int init(std::vector<int> const & genes);
     std::vector<double> getWTKScomb(std::vector<std::string> & q_up, std::vector<std::string> & q_dn);
-    std::vector<double> getWTKScomb_old(std::vector<std::string> & q_up, std::vector<std::string> & q_dn);
 
     size_type const NSIG;
     size_type const NSKIP;
@@ -98,7 +97,7 @@ CMAP2Updated::init(std::vector<int> const & genes)
 std::vector<double>
 CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::string> & q_dn)
 {
-    std::cerr << "Will process " << NSIG - NSKIP << " signatures, [" << NSKIP << ',' << NSIG << ")\n";
+    std::cout << "Will process " << NSIG - NSKIP << " signatures, [" << NSKIP << ',' << NSIG << ")\n";
 
     assert(q_up.size() == q_dn.size());
     auto const NQRY = q_up.size();
@@ -129,9 +128,19 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
     auto const q_up_indexed = q_indexer(q_up);
     auto const q_dn_indexed = q_indexer(q_dn);
 
+    typedef union
+    {
+        double f64;
+        std::uint64_t i64;
+    } packed_score_ix_t;
+//    using packed_score_ix_t = std::uint64_t;
+
+    //using score_ix_t = std::pair<double, score_index_t>;
+    using score_ix_t = packed_score_ix_t;
+
     typedef struct
     {
-        std::vector<std::pair<double, score_index_t>> scores;
+        std::vector<score_ix_t> scores;
         double sum;
     } query_state_t;
 
@@ -142,6 +151,9 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
 
     for (auto qix = 0u; qix < NQRY; ++qix)
     {
+        q_up_states[qix].scores.reserve(200);
+        q_dn_states[qix].scores.reserve(200);
+
         for (auto const gene_ix : q_up_indexed[qix])
         {
             gene_buckets[gene_ix].push_back(&q_up_states[qix]);
@@ -152,19 +164,10 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
         }
     }
 
-    for (auto six = 0u; six < NSIG; ++six)
+    for (auto six = NSKIP; six < NSIG; ++six)
     {
-        auto sigs = m_cmap_lib.loadFromDoubleFile(PATH + "scoresBySig", six * NGENES, NGENES);
+        auto sigs = m_cmap_lib.loadFromDoubleFile(PATH + "scoresBySigSortedAbs", six * NGENES, NGENES);
 
-//        namespace q = ::cpplinq;
-//        auto ranks = m_cmap_lib.loadFromIntFile(PATH + "ranksBySig", six * NGENES, NGENES);
-//        auto inv_ranks =
-//            q::from(ranks)
-//            >> q::zip_with(q::detail::int_range(0, INT_MAX))
-//            >> q::orderby_ascending([](std::pair<int, int> const & ri){ return ri.first; })
-//            >> q::select([](std::pair<int, int> const & ri){ return ri.second; })
-//            >> q::to_vector(NGENES)
-//            ;
         auto _ranks = m_cmap_lib.loadFromIntFile(PATH + "ranksBySigInv", six * NGENES / 2, NGENES / 2);
         auto inv_ranks = (std::uint16_t const *)_ranks.data();
 
@@ -180,11 +183,19 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
         for (auto gix = 0; gix < NGENES; ++gix)
         {
             auto inv_rank = inv_ranks[gix];
-            auto score = std::abs(sigs[inv_rank]);
+            auto score = sigs[gix];
 
             for (auto q_state_p : gene_buckets[inv_rank])
             {
-                q_state_p->scores.emplace_back(score, gix);
+                //q_state_p->scores.emplace_back(score, gix);
+                packed_score_ix_t sx;
+//                sx.score.f51 = (std::uint64_t)score >> 13;
+//                sx.ix.i14 =
+                sx.f64 = score;
+                sx.i64 >>= 13;
+                //sx &= ((-1ULL) >> 14);
+                sx.i64 |= ((std::uint64_t)gix << 50);
+                q_state_p->scores.push_back(sx);
                 q_state_p->sum += score;
             }
         }
@@ -202,14 +213,19 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
                     double _max = 0.;
                     int prev = 0; // rank indices are 1-based
 
-                    for (auto const & sr : q_state.scores)
+                    for (auto sr : q_state.scores)
                     {
-                        _acc += (sr.second - prev - 0) * penalty;
+                        auto ix = sr.i64 >> 50;
+                        sr.i64 <<= 13;
+                        //sr.i64 &= 0x7FFFFFFFE0000000ULL; //((-1ULL) >> 1);
+                        sr.i64 &= ((-1ULL) >> 1);
+                        auto sc = sr.f64;
+                        _acc += (ix - prev - 0) * penalty;
                         _min = std::min(_min, _acc);
 
-                        prev = sr.second + 1;
+                        prev = ix + 1;
 
-                        _acc += sr.first / q_state.sum;
+                        _acc += sc / q_state.sum;
                         _max = std::max(_max, _acc);
                     }
                     if (_max > std::abs(_min))
@@ -225,146 +241,6 @@ CMAP2Updated::getWTKScomb(std::vector<std::string> & q_up, std::vector<std::stri
 
             double wtks_up = wtks_calc(q_up_states[qix]);
             double wtks_dn = wtks_calc(q_dn_states[qix]);
-
-            auto wtks = (wtks_dn * wtks_up < 0.) ? (wtks_up - wtks_dn) / 2 : 0.;
-            ret.push_back(wtks);
-
-            if (UNLIKELY(m_gt != nullptr))
-            {
-                auto ref = (*m_gt)[(six - NSKIP) * NQRY + qix];
-                if (std::abs(ref - wtks) >= 0.001)
-                {
-                    std::cout << "Sig: " << six + 1 << ", Query: " << qix + 1 \
-                        << " Ref: " << ref << " WTKS: " << wtks << " up/dn " << wtks_up << ' ' << wtks_dn << '\n';
-                }
-            }
-        }
-    }
-
-    return ret;
-}
-
-std::vector<double>
-CMAP2Updated::getWTKScomb_old(std::vector<std::string> & q_up, std::vector<std::string> & q_dn)
-{
-    std::cerr << "Will process " << NSIG - NSKIP << " signatures, [" << NSKIP << ',' << NSIG << ")\n";
-
-    const auto time0 = timestamp();
-
-    assert(q_up.size() == q_dn.size());
-    auto const NQRY = q_up.size();
-
-    auto q_indexer = [this](std::vector<std::string> & qry)
-        {
-            namespace q = ::cpplinq;
-            return q::from(qry)
-                >> q::select([this](std::string const & s)
-                    {
-                        auto q_parsed = parse_query(s);
-                        std::transform(q_parsed.begin(), q_parsed.end(), q_parsed.begin(),
-                            [this](int gene_id){ return this->m_gene_to_idx.at(gene_id); });
-                        return std::vector<std::uint16_t>(q_parsed.cbegin(), q_parsed.cend());
-                        //return q_parsed;
-                    })
-                >> q::to_vector();
-        };
-
-    auto const q_up_indexed = q_indexer(q_up);
-    auto const q_dn_indexed = q_indexer(q_dn);
-
-    std::vector<double> sig;
-    std::vector<int> ranks;
-
-    std::vector<double> ret;
-    ret.reserve(NQRY * NSIG);
-
-    typedef union
-    {
-        struct
-        {
-            std::uint16_t score_ix;
-            std::uint16_t rank;
-        } rs;
-        std::uint32_t packed;
-    } rank_score_t;
-
-    std::vector<rank_score_t> hranks[NBUCKET];
-    for (auto & bucket : hranks)
-    {
-        bucket.reserve(250 / (2 * NBUCKET));
-    }
-
-    for (auto six = NSKIP; six < NSIG; ++six)
-    {
-        sig = m_cmap_lib.loadFromDoubleFile(PATH + "scoresBySig", six * NGENES, NGENES);
-        ranks = m_cmap_lib.loadFromIntFile(PATH + "ranksBySig", six * NGENES, NGENES);
-        double const * sig_p = sig.data();
-        unsigned int const * ranks_p = (unsigned int const *)ranks.data();
-        // cache i/o ???
-
-        for (auto qix = 0u; qix < NQRY; ++qix)
-        {
-            auto & q_up_indices = q_up_indexed[qix];
-            auto & q_dn_indices = q_dn_indexed[qix];
-
-            auto const QUSIZE = q_up_indices.size();
-            auto const QDSIZE = q_dn_indices.size();
-
-            auto calc_wtks = [&hranks, ranks_p, sig_p](size_type const QSIZE, std::vector<std::uint16_t> const & q_indices)
-            {
-                double Sum_abs_scores = 0.;
-
-                for (auto & bucket : hranks)
-                {
-                    bucket.clear();
-                }
-                for (auto const ix : q_indices)
-                {
-                    auto abs_score = std::abs(sig_p[ix]);
-                    Sum_abs_scores += abs_score;
-                    hranks[ranks_p[ix] / RANK_PER_BUCKET].emplace_back((rank_score_t){ix, ranks_p[ix]});
-                }
-                for (auto & bucket : hranks)
-                {
-                    std::uint32_t * bucket_p = (std::uint32_t *)bucket.data();
-                    std::sort(bucket_p, bucket_p + bucket.size(),
-                        [](std::uint32_t p, std::uint32_t q)
-                        { return p < q;});
-                }
-
-                double const penalty = -1. / (NGENES - QSIZE);
-                double wtks = 0.;
-                double _acc = 0.;
-                double _min = 0.;
-                double _max = 0.;
-                int prev = 0; // rank indices are 1-based
-
-                for (auto const & bucket : hranks)
-                {
-                    for (auto const & rs : bucket)
-                    {
-                        _acc += (rs.rs.rank - prev - 1) * penalty;
-                        _min = std::min(_min, _acc);
-
-                        prev = rs.rs.rank;
-
-                        _acc += std::abs(sig_p[rs.rs.score_ix]) / Sum_abs_scores;
-                        _max = std::max(_max, _acc);
-                    }
-                }
-                if (_max > std::abs(_min))
-                {
-                    wtks = _max;
-                }
-                else
-                {
-                    wtks = _min;
-                }
-                return wtks;
-            };
-
-            auto const wtks_up = calc_wtks(QUSIZE, q_up_indices);
-            auto const wtks_dn = calc_wtks(QDSIZE, q_dn_indices);
 
             auto wtks = (wtks_dn * wtks_up < 0.) ? (wtks_up - wtks_dn) / 2 : 0.;
             ret.push_back(wtks);
