326f25ecf0e15e0a1f6a250951b1548884fbf1ee
Wojciech Migda <wojtek.golf@interia.pl>
2017-02-16 18:01:19 +0100

    chrt in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-./run_conc ../data/offline_query_up_n250.csv ../data/offline_query_down_n250.csv ../data/o_wtks_conc.bin
+./run_conc ../data/offline_query_up_n250.csv ../data/offline_query_down_n250.csv ../data/o_wtks_conc.bin #&
+
+exit
+
+PID=$!
+chrt -a -b -p 0 $PID
+chrt -p $PID
+wait $PID
