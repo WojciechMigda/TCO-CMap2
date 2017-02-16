d31c3e4b21dbf79b46389048704f827b67e1105d
Wojciech Migda <wojtek.golf@interia.pl>
2017-02-16 18:17:23 +0100

    run250.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./run_conc ../data/offline_query_up_n250.csv ../data/offline_query_down_n250.csv ../data/o_wtks_conc.bin #&
+./run_conc ${@} #&
 
 exit
 
---
+++ run250.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. ./run.sh ../data/offline_query_up_n250.csv ../data/offline_query_down_n250.csv ../data/o_wtks_conc.bin
